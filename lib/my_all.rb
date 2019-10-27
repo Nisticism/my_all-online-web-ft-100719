71be1117d4495c3de18e9f6f793360734dcf1f8a
Hans Foster <fosterhans@gmail.com>
2019-10-27 19:16:39 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -4,7 +4,13 @@ def my_all?(collection)
   i = 0 
   block_return_values = []
   while i < colleciton.length 
-    block_return_values << -yield(colleciton[i])
+    block_return_values << yield(colleciton[i])
     i = i + 1 
+  end
+  
+  if block_return_values.include?(false)
+    false
+  else 
+    TRUE
   end
 end
